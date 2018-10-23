413c9b2045c57fa253707ce8c6725d1057b017f2
ahmadowais123 <owais.khan2@mail.mcgill.ca>
2018-10-23 14:31:00 -0400

    Read all working, test 1 failing on last part

--- Assignment2/src/a2_lib.c
+++ Assignment2/src/a2_lib.c
@@ -5,32 +5,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 #include "a2_lib.h"
 
-#define numberPods 256
-#define keySize 32
-#define memorySize 73000000000
-
-data *memory;
-int fd;
-
-int main(int argc, char **argv) {
-	if(kv_store_create(DATA_BASE_NAME) == -1) {
-		exit(1);
-	}
-
-//	kv_store_write("0", "value1key0");
-//	kv_store_write("0", "value2key0");
-//	kv_store_write("255", "value1key255");
-//	kv_store_write("255", "value2key255");
-
-	printf("%s\n",kv_store_read("0"));
-	printf("%s\n",kv_store_read("0"));
-	printf("%s\n",kv_store_read("255"));
-	printf("%s\n",kv_store_read("255"));
-}
-
-int  kv_store_create(const char *name) {
+int kv_store_create(const char *name) {
 	fd = shm_open(name, O_RDWR | O_CREAT, S_IRWXU);
 
 	if(fd == -1) {
@@ -38,69 +16,109 @@ int  kv_store_create(const char *name) {
 		return -1;
 	}
 
-	ftruncate(fd, memorySize);
-	memory = (data *)mmap(NULL, memorySize, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	size_t size = sizeof(data) + (numberPods*numberPods*pairSize);
+	ftruncate(fd, size);
+	memory = (char *)mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	close(fd);
 
 	if(memory == MAP_FAILED) {
 		perror("Failed to map memory");
 		exit(1);
 	}
 
-//	for(int i=0; i<256; i++) {
-//		memory->writeCounters[i] = 0;
-//		memory->readCounters[i] = 0;
-//	}
+
+	data *bookKeeping = (data *)memory;
+	for(int i=0; i<256; i++) {
+		bookKeeping->writeCounters[i] = 0;
+		bookKeeping->readCounters[i] = 0;
+	}
 
 	return 0;
 }
 
-int hashFunction(char *key) {
-	int hash = 5381;
+unsigned long hashFunction(const char *key) {
+	unsigned long hash = 5381;
 	int c;
 
-	while (c = *key++) {
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	while(c = *key++) {
+	    hash = ((hash << 5) + hash) + c;
 	}
 
-	return hash;
+    hash = (hash>0) ? hash : -(hash);
+	return hash%256;
 }
 
 int kv_store_write(const char *key, const char *value) {
-	int index = atoi(key);
-	long pairOffset = sizeof(kvpair) * memory->writeCounters[index];
-	long podOffset = sizeof(kvpair)*numberPods*index;
+	int index = hashFunction(key);
+
+	data *bookKeeping = (data *)memory;
+	size_t pairOffset = pairSize * bookKeeping->writeCounters[index];
+	size_t podOffset = podSize*index;
 
 
 	memcpy(memory+sizeof(data)+podOffset+pairOffset, key, keySize);
-	memcpy(memory+sizeof(data)+podOffset+pairOffset+keySize, value, numberPods);
+	memcpy(memory+sizeof(data)+podOffset+pairOffset+keySize, value, valueSize);
 
-	memory->writeCounters[index] = memory->writeCounters[index] + 1;
-	memory->writeCounters[index] = memory->writeCounters[index]%numberPods;
+	bookKeeping->writeCounters[index]++;
+	bookKeeping->writeCounters[index] = bookKeeping->writeCounters[index]%numberPods;
 
 	return 0;
 }
 
 char *kv_store_read(const char *key) {
-	int index = atoi(key);
-	int pairOffset = sizeof(kvpair)*memory->readCounters[index];
-	int podOffset = sizeof(kvpair)*numberPods*index;
+    int index = hashFunction(key);
+    char *duplicateValue;
 
-	char *dupString = (char *)malloc(sizeof(char)*256);
-	memcpy(dupString, memory+sizeof(data)+podOffset+pairOffset+keySize, 256);
+    data *bookKeeping = (data *)memory;
+    size_t podOffset = podSize*index;
 
-	memory->readCounters[index] = memory->readCounters[index] + 1;
-	memory->readCounters[index] = memory->readCounters[index]%256;
+    for(int i=0;i<256;i++) {
+        size_t pairOffset = pairSize*bookKeeping->readCounters[index];
 
-	return dupString;
+        if(memcmp(memory + sizeof(data) + podOffset + pairOffset, key, strlen(key)) == 0) {
+            duplicateValue = strdup(memory + sizeof(data) + podOffset + pairOffset + keySize);
+            bookKeeping->readCounters[index]++;
+            bookKeeping->readCounters[index] = bookKeeping->readCounters[index]%256;
+            return duplicateValue;
+        }
+
+        bookKeeping->readCounters[index]++;
+        bookKeeping->readCounters[index] = bookKeeping->readCounters[index]%256;
+    }
+
+    return NULL;
 };
 
 char **kv_store_read_all(const char *key) {
-	char **string;
-	return string;
+    char **allStrings = malloc(sizeof(char*));
+    char *nextValue;
+
+    int count = 0;
+    int index = hashFunction(key);
+    size_t podOffset = podSize * index;
+
+    for(int i=0; i<256; i++) {
+        size_t pairOffset = i*pairSize;
+
+        if(strcmp(memory+sizeof(data)+podOffset+pairOffset, "") == 0) {
+            if(i == 0) return NULL;
+            free(allStrings[count]);
+            allStrings[count] = NULL;
+            return allStrings;
+        } else if(strcmp(memory+sizeof(data)+podOffset+pairOffset, key) == 0) {
+            nextValue = strdup(memory + sizeof(data) + podOffset + pairOffset + keySize);
+            allStrings[count] = nextValue;
+            count++;
+            allStrings = realloc(allStrings, sizeof(char*)*(count+1));
+        }
+    }
+	allStrings[count] = NULL;
+    return allStrings;
 }
 
 void kv_delete_db() {
-	if(munmap(memory, memorySize) == -1){
+	long size = sizeof(data) + (numberPods*podSize);
+	if(munmap(memory, size) == -1){
 		perror("Failed to delete database");
 		exit(1);
 	}
--- Assignment2/src/a2_lib.h
+++ Assignment2/src/a2_lib.h
@@ -7,6 +7,15 @@
 #define KEY_MAX_LENGTH   32
 #define VALUE_MAX_LENGTH 256
 
+#define numberPods 256
+#define pairSize 288
+#define keySize 32
+#define valueSize 256
+#define podSize 73728
+
+char *memory;
+int fd;
+
 typedef struct {
     char key[KEY_MAX_LENGTH];
     char value[VALUE_MAX_LENGTH];
@@ -17,7 +26,6 @@ typedef struct {
     int readCounters[256];
 }data;
 
-kvpair *pointer;
 
 /* if you use the default interface uncomment below */
 extern int  kv_store_create(const char *name);
@@ -25,7 +33,7 @@ extern int  kv_store_write(const char *key, const char *value);
 extern char *kv_store_read(const char *key);
 extern char **kv_store_read_all(const char *key);
 extern void kv_delete_db();
-extern int hashFunction(char *key);
+extern unsigned long hashFunction(const char *key);
 
 /* if you write your own interface, please fill the following adaptor */
 //int    (*kv_store_create)(const char*)             = NULL;
